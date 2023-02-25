cbb80d74cc07d009ffeb705ea5aee5f0ef11f800
tarun968 <tarunsinha968@gmail.com>
2023-02-25 18:20:12 +0530

    backend path

--- src/backendjoin/backend.js
+++ src/backendjoin/backend.js
@@ -1,2 +1,2 @@
-export const BACKEND = 'https://backendfarmdropmern2.onrender.com/'
+export const BACKEND = 'https://backendfarmdropmern2.onrender.com'
 // export const BACKEND = 'http://localhost:5000'
